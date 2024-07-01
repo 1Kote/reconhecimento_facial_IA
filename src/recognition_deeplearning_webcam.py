ad65a5bf7fa32406cf2d7294b9f834fafd97e100
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2024-06-30 23:22:54 -0300

    feat: display recognized faces in the image

--- src/recognition_deeplearning_webcam.py
+++ src/recognition_deeplearning_webcam.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import pickle
 import face_recognition
+from helper_functions import resize_video
 
 # Nome do arquivo pickle onde as codificações faciais estão armazenadas
 pickle_name = "face_encodings_custom.pickle"
@@ -43,3 +44,41 @@ def recognize_faces(image, list_encodings, list_names, resizing=0.25, tolerance=
     face_locations = face_locations / resizing
     
     return face_locations.astype(int), face_names, conf_values
+
+# Função para exibir os rostos reconhecidos na imagem
+def show_recognition(frame, face_locations, face_names, conf_values):
+    for face_loc, name, conf in zip(face_locations, face_names, conf_values):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+        
+        conf = "{:.8f}".format(conf)
+        
+        # Desenha o nome e a caixa delimitadora ao redor do rosto
+        cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 0.7, (20, 255, 0), 2, lineType=cv2.LINE_AA)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (20, 255, 0), 4)
+        
+        if name != "Not identified":
+            cv2.putText(frame, conf, (x1, y2 + 15), cv2.FONT_HERSHEY_DUPLEX, 0.5, (20, 255, 0), 1, lineType=cv2.LINE_AA)
+    
+    return frame
+
+cam = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cam.read()
+    
+    # Redimensiona o quadro se uma largura máxima for especificada
+    if max_width is not None:
+        video_width, video_height = resize_video(frame.shape[1], frame.shape[0], max_width)
+        frame = cv2.resize(frame, (video_width, video_height))
+    
+    # Reconhece rostos no quadro atual
+    face_locations, face_names, conf_values = recognize_faces(frame, list_encodings, list_names, 0.25)
+    processed_frame = show_recognition(frame, face_locations, face_names, conf_values)
+    
+    # Exibe o quadro processado
+    cv2.imshow("Recognizing faces", frame)
+    cv2.waitKey(1)
+
+print("Completed!")
+cam.release()
+cv2.destroyAllWindows()
